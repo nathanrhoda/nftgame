76a7ad19e7e698ffc1d4ffc40b973db173a22dcc
Nathan Rhoda <nathan.rhoda@gmail.com>
2022-05-02 16:26:07 +0200

    Implemented mint function

--- contracts/NFTGame.sol
+++ contracts/NFTGame.sol
@@ -7,17 +7,28 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract NFTGame is ERC721URIStorage, Ownable
 {
     struct Car
-    {
-        uint256 fuel;
-        string brand;
+    {        
         uint256 speed;
+        uint256 fuel;        
+        uint256 refuel;
+        uint256 currentRange;
     }
 
     mapping (uint256=>Car) private carDetails;
+    uint256 tokenCounter=0;
 
     constructor(string memory name, string memory symbol)
         ERC721(name, symbol)
     {
 
     }    
+
+    function mint(uint256 speed, uint256 fuel, uint256 refuel, uint range) 
+        public onlyOwner
+    {
+        uint256 newid = tokenCounter;
+        carDetails[newid] = Car(speed, fuel, refuel, range);
+        _safeMint(msg.sender, newid);
+        tokenCounter++;
+    }
 }
