d59921357cf6604f73a778a63db38a106b167335
Nathan Rhoda <nathan.rhoda@gmail.com>
2022-05-02 16:16:50 +0200

    Basic structure of NFT

--- contracts/NFTGame.sol
+++ contracts/NFTGame.sol
@@ -4,8 +4,20 @@ pragma solidity >=0.4.22 <0.9.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-// contract NFTGame is ERC721URIStorage, Ownable
-// {
+contract NFTGame is ERC721URIStorage, Ownable
+{
+    struct Car
+    {
+        uint256 fuel;
+        string brand;
+        uint256 speed;
+    }
 
+    mapping (uint256=>Car) private carDetails;
 
-// }
+    constructor(string memory name, string memory symbol)
+        ERC721(name, symbol)
+    {
+
+    }    
+}
